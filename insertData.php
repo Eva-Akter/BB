272be3116c9ae5e9d0006ffaae801e8d158bdf81
Eva-Akter <128277505+Eva-Akter@users.noreply.github.com>
2023-06-20 22:10:48 +0600

    Add files via upload

---
+++ insertData.php
@@ -0,0 +1,24 @@
+
+<?php
+include "dbconnect.php";
+$name=$_POST['f_name'];
+$roll=$_POST['f_roll'];
+$email=$_POST['f_email'];
+
+$sql = "INSERT INTO student (id, name,roll,email) 
+		VALUES (NULL, '$name', '$roll', '$email')";
+
+		
+		if($conn->query($sql))
+		{
+			echo "data inserted successfully";
+		}
+		else
+		{
+			echo "insertion failed";
+		}
+		
+		
+		$conn->close();
+?>
+
---
+++ insertForm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>	
+	<center>
+		<h1>Insert Form</h1>
+		<form method="POST" action="insertData.php">
+			<label>Name</label>
+			<input type="text" placeholder="Enter name" name="f_name"> <br>
+			<label>Roll</label>
+			<input type="text" placeholder="Enter roll" name="f_roll"> <br>
+			<label>Email</label>
+			<input type="email"  placeholder="Enter email" name="f_email" > <br>
+			<input type="submit" value="INSERT">
+		</form>
+	</center>	
+	
+</body>
+</html>
